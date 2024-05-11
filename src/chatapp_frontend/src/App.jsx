94fd7d6b06858179bf89be1b09faf30fc9ed0a98
harshedabdulla <harshed2004@gmail.com>
2024-05-11 16:02:16 +0530

    fixes

--- src/chatapp_frontend/src/App.jsx
+++ src/chatapp_frontend/src/App.jsx
@@ -10,9 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Connect />} />
+        <Route path="/home" element={<Chat />} />
         <Route path="/chats" element={<Chats />} />
-        <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   )
